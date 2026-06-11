89fbdf80ac0ea45f7d49d506bf4e2a938dea3852
Jeff Stano <jeff@stano.com>
2026-06-11 14:09:57 -0400

    git commit -m "Use XSD schema validation instead of DTD validation
      Switch from DTD validation to proper W3C XML Schema validation. Load
      schema.xsd from classpath and apply it via JAXP, providing better
      type checking, required-field enforcement, and error reporting. Also
      fixed incorrect default value for primary key cluster attribute."

--- schema-parser/src/main/java/com/stano/schema/parser/xmlparser/XMLSchemaParser.java
+++ schema-parser/src/main/java/com/stano/schema/parser/xmlparser/XMLSchemaParser.java
@@ -4,8 +4,11 @@ import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.BufferedReader;
 
 public class XMLSchemaParser {
@@ -23,9 +26,13 @@ public class XMLSchemaParser {
 
   private XMLReader createXMLReader() {
     try {
+      // Load XSD schema from classpath
+      SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      Schema schema = schemaFactory.newSchema(getClass().getResource("/schema.xsd"));
+
       // Allocate and configure JAXP SAX parser factory
       SAXParserFactory factory = SAXParserFactory.newInstance();
-      factory.setValidating(true);
+      factory.setSchema(schema);
       factory.setNamespaceAware(true);
       factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
       factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
